c4e0140f94b26738e67e2f691b97680b9f3973b9
Nicole Branagan <nicolecarolinebranagan@gmail.com>
2018-10-24 23:21:06 -0400

    Add a color selector

--- src/Constants.js
+++ src/Constants.js
@@ -1,3 +1,5 @@
+const KeyMirror = (list) => list.reduce((prev, next) => {prev[next] = next; return prev;}, {});
+
 export const HIRES_WIDTH = 280;
 export const HIRES_BYTE_WIDTH = 320;
 export const HIRES_HEIGHT = 192;
@@ -7,3 +9,35 @@ export const HIRES_WHITE = "#FFFFFF";
 export const HIRES_BLUE = "#0055FF";
 export const HIRES_ORANGE = "#FF5500";
 export const MONO = "#00DD00";
+
+export const Colors = KeyMirror([
+  'VIOLET',
+  'GREEN',
+  'WHITE',
+  'BLUE',
+  'ORANGE',
+  'BLACK'
+])
+
+export const COLOR_COLORS = [
+  Colors.WHITE,
+  Colors.VIOLET,
+  Colors.GREEN,
+  Colors.BLUE,
+  Colors.ORANGE,
+  Colors.BLACK
+];
+
+export const MONO_COLORS = [
+  Colors.WHITE,
+  Colors.BLACK
+];
+
+export const ColorCodes = {
+  [Colors.WHITE]: [1, 1],
+  [Colors.VIOLET]: [0, 1],
+  [Colors.GREEN]: [1, 0],
+  [Colors.BLUE]: [0, 1],
+  [Colors.ORANGE]: [1, 0],
+  [Colors.BLACK]: [0, 0],
+};
--- src/components/HiresContainer.js
+++ src/components/HiresContainer.js
@@ -1,5 +1,5 @@
 import React, { PureComponent } from 'react';
-import { HIRES_BYTE_WIDTH, HIRES_HEIGHT } from '../Constants';
+import { HIRES_BYTE_WIDTH, HIRES_HEIGHT, Colors, COLOR_COLORS, MONO_COLORS, ColorCodes } from '../Constants';
 import HiresRow from './HiresRow';
 
 export default class HiresContainer extends PureComponent {
@@ -9,26 +9,72 @@ export default class HiresContainer extends PureComponent {
     this.state = {
       data: Array(HIRES_HEIGHT).fill(Array(HIRES_BYTE_WIDTH).fill(0)),
       color: true,
+      selectedColor: Colors.WHITE,
     };
   }
 
   onToggleColor = (event) => {
     this.setState({
       color: event.target.checked,
+      selectedColor: event.target.checked ? this.state.selectedColor : Colors.BLACK,
     });
   }
 
   onClick(row, column) {
-    const { data } = this.state;
-    const byte = Math.floor(column / 7);
-    const byteOffset = byte * 8 + column % 7 + 1;
+    const { data, selectedColor, color } = this.state;
+    if (color) {
+      column = Math.floor(column / 2) * 2;
+    }
+    console.log(column);
+    const byte = Math.floor(column / 7) * 8;
+    const byteOffset = byte + column % 7 + 1;
     const newData = [...data];
     newData[row] = [...data[row]];
-    const bit = data[row][byteOffset];
-    newData[row][byteOffset] = bit ? 0 : 1;
+    if (color) {
+      const codes = ColorCodes[selectedColor];
+      if (selectedColor === Colors.VIOLET || selectedColor === Colors.GREEN) {
+        newData[row][byte] = 0;
+      } else if (selectedColor === Colors.VIOLET || selectedColor === Colors.GREEN) {
+        newData[row][byte] = 1;
+      }
+      newData[row][byteOffset] = codes[0];
+
+      if ((byteOffset + 1) % 8 === 0) {
+        if (selectedColor === Colors.VIOLET || selectedColor === Colors.GREEN) {
+          newData[row][byteOffset+1] = 0;
+        } else if (selectedColor === Colors.VIOLET || selectedColor === Colors.GREEN) {
+          newData[row][byteOffset+1] = 1;
+        }
+        newData[row][byteOffset+2] = codes[1];
+      } else {
+        newData[row][byteOffset+1] = codes[1];
+      }
+    } else {
+      newData[row][byteOffset] = selectedColor === Colors.BLACK ? 0 : 1;
+    }
+
     this.setState({
       data: newData,
     });
+  }
+
+  renderColorOptions() {
+    const { color, selectedColor } = this.state;
+    const map = color ? COLOR_COLORS : MONO_COLORS;
+    return (
+      <span>
+        {map.map(color => <span key={color}>
+          <input 
+            type='radio' 
+            name='colorSelect' 
+            value={color} 
+            checked={color === selectedColor}
+            onChange={(e) => this.setState({selectedColor: e.target.value})}
+          />
+          {color}
+        </span>)}
+      </span>
+    )
   }
 
   render() {
@@ -37,7 +83,8 @@ export default class HiresContainer extends PureComponent {
     return (
       <div>
         <div>
-          Color: <input type="checkbox" checked={color} onChange={this.onToggleColor} />
+          <span>Color/Mono: <input type="checkbox" checked={color} onChange={this.onToggleColor} /></span>
+          <span>Color: {this.renderColorOptions()} </span>
         </div>
         {[...Array(HIRES_HEIGHT).keys()].map(row => (
           <HiresRow
